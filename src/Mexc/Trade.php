08f07f0b09d4906999654abe66811b2bce5e0c9a
aliromero <aliyeketaz@gmail.com>
2024-02-04 13:21:33 +0330

    add trade

---
+++ src/Mexc/Trade.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Romero\Mexc\Mexc;
+
+class Trade extends Time
+{
+	public static function trade(string $symbol, string $side, string $type, string $quantity, string $price): array|bool
+	{
+
+
+		$buildQuery = [
+			'symbol' => $symbol,
+			'side' => $side,
+			'type' => $type,
+			'quantity' => $quantity,
+			'price' => $price,
+			'recvWindow' => 10000,
+			'timestamp' => Time::time(5000)
+		];
+
+		$url = MEXC_CONFIG['MEXC_URL_API'] . '/capital/withdraw/apply?' . BuildHttpQuery::build($buildQuery) . '&signature=' . Signature::signature($buildQuery);
+		$ch = curl_init($url);
+
+		curl_setopt_array($ch, [
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_HTTPHEADER => [
+				'X-MEXC-APIKEY: ' . MEXC_CONFIG['MEXC_API_ACCESS_KEY'] . ''
+			],
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false,
+		]);
+
+		$res = curl_exec($ch);
+
+		if (!$res) {
+			curl_close($ch);
+			return false;
+		}
+
+		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		if ($status_code != 200)
+			return json_decode($res, true);
+
+		return json_decode($res, true);
+	}
+
+
+}
