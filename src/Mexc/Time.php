87395fa547a6a4d73a709b8b65c78e8c7d9c7cd5
aliromero <aliyeketaz@gmail.com>
2024-02-05 15:54:50 +0330

    update time

--- src/Mexc/Time.php
+++ src/Mexc/Time.php
@@ -4,9 +4,12 @@ namespace Romero\Mexc\Mexc;
 
 class Time
 {
+
 	public static function time(int $recvWindow): int
 	{
-		$ts = round(microtime(true) * 1000);
+        date_default_timezone_set('Asia/Tehran');
+
+        $ts = round(microtime(true) * 1000);
 		$ts -= $ts % $recvWindow;
 		return $ts;
 	}
