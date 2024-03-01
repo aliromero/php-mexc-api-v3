7f2fce796f02ecc31cf9f2ab52210c234d052b45
aliromero <aliyeketaz@gmail.com>
2024-03-02 01:58:51 +0330

    update slug

--- src/Mexc/Withdraw.php
+++ src/Mexc/Withdraw.php
@@ -6,8 +6,7 @@ class Withdraw extends Time
 {
 	public static function withdraw(string $coin, string $network, string $address, string $amount, string $withdrawOrderId): array|bool
 	{
-		$coin = strtoupper($coin ?? 'WEMIX');
-		$network = strtoupper($network ?? 'WEMIX');
+
 
 		$buildQuery = [
 			'coin' => $coin,
