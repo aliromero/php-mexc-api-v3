33acf513032d020fdc845c99e6074a374cf6f3db
aliromero <aliyeketaz@gmail.com>
2024-03-02 02:46:47 +0330

    update slug

--- src/Mexc/BuildHttpQuery.php
+++ src/Mexc/BuildHttpQuery.php
@@ -7,11 +7,6 @@ class BuildHttpQuery
 	public static function build(array $params): string
 	{
 		ksort($params);
-
-        $params = http_build_query($params, '', '&', PHP_QUERY_RFC3986);
-        $params = str_replace('+', '%20', $params);
-
-
-		return $params;
+		return http_build_query($params);
 	}
 }
