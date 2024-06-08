92e296680d37433fc41507de172098cc4ddf72c9
aliromero <aliyeketaz@gmail.com>
2024-06-08 13:05:49 +0330

    add history

--- src/Mexc/Account.php
+++ src/Mexc/Account.php
@@ -69,4 +69,50 @@ class Account extends Time
 
 		return $balances;
 	}
+    public static function getDepositHistory(string $coin = null,
+                                             string $status = null,
+                                             string $startTime = null,
+                                             string $endTime = null,
+                                             string $limit = null,
+    ): array|bool
+    {
+        $coin = strtoupper($coin ?? '');
+
+        $buildQuery = [
+            'coin' => $coin,
+            'status' => $status,
+            'startTime' => $startTime,
+            'endTime' => $endTime,
+            'limit' => $limit,
+            'timestamp' => Time::time(5000)
+        ];
+
+        $url = config('my_settings.MEXC_URL_API') . '/capital/deposit/hisrec?' . BuildHttpQuery::build($buildQuery) . '&signature=' . Signature::signature($buildQuery);
+        $ch = curl_init($url);
+
+        curl_setopt_array($ch, [
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => [
+                'X-MEXC-APIKEY: ' . config('my_settings.MEXC_API_ACCESS_KEY') . ''
+            ],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+        ]);
+
+        $res = curl_exec($ch);
+
+        if (!$res) {
+            curl_close($ch);
+            return false;
+        }
+
+        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($status_code != 200)
+            return json_decode($res, true);
+
+        return json_decode($res, true);
+    }
+
 }
